3c9b79331db71748ad1e4a89e9b23c2e40f3f9e2
deltagare <deltagare@LEXJONKAF27>
2020-10-06 15:58:57 +0200

    Fixed Error In Program

--- TodoItApp/Program.cs
+++ TodoItApp/Program.cs
@@ -5,21 +5,13 @@ namespace TodoItApp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Person p;
-
-
-            Person[] personArray = new Person[3];
-            personArray[0] = new Person(123, "Kalle", "Karlsson");
-            personArray[1] = new Person(321, "Niklas", "Borjeson");
-            personArray[2] = new Person(444, "Gustav", "Areaa");
-
-            foreach (Person person in personArray)
+        
+        
+            static void Main(string[] args)
             {
-                Console.WriteLine(person.FirstName);
+                Console.WriteLine("Hello World!");
             }
-            Console.ReadLine();
         }
     }
-}
+    
+
